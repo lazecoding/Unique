72fef286857bd16e3602db3d12596fbbb8f29d6e
lazecoding <15062270668@163.com>
2020-12-04 21:09:21 +0800

    feat: Spring生命周期 Listener扩展

---
+++ src/main/java/personal/unique/common/BeanUtil.java
@@ -0,0 +1,68 @@
+package personal.unique.common;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.BeansException;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.ApplicationContextAware;
+import org.springframework.stereotype.Component;
+
+/**
+ * @author: lazecoding
+ * @date: 2020/12/4 20:36
+ * @description:
+ */
+@Component("BeanUtil")
+public class BeanUtil implements ApplicationContextAware {
+    private Logger logger = LoggerFactory.getLogger(this.getClass());
+
+    private static ApplicationContext applicationContext;
+
+    @Override
+    public void setApplicationContext(ApplicationContext applicationContext) throws BeansException {
+        if (BeanUtil.applicationContext == null) {
+            BeanUtil.applicationContext = applicationContext;
+        }
+        logger.debug("applicationContext Ready");
+    }
+
+    /**
+     * 获取applicationContext
+     *
+     * @return
+     */
+    public static ApplicationContext getApplicationContext() {
+        return applicationContext;
+    }
+
+    /**
+     * 通过name获取 Bean.
+     *
+     * @param name
+     * @return
+     */
+    public static Object getBean(String name) {
+        return getApplicationContext().getBean(name);
+    }
+
+    /**
+     * 通过class获取Bean.
+     *
+     * @param clazz
+     * @return
+     */
+    public static <T> T getBean(Class<T> clazz) {
+        return getApplicationContext().getBean(clazz);
+    }
+
+    /**
+     * 通过name,以及Clazz返回指定的Bean
+     *
+     * @param name
+     * @param clazz
+     * @return
+     */
+    public static <T> T getBean(String name, Class<T> clazz) {
+        return getApplicationContext().getBean(name, clazz);
+    }
+}
