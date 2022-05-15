17f7c1a0f843e85c2b2f23be016adaa0eb4fae00
lazecoding <15062270668@163.com>
2022-05-15 08:57:30 +0800

    refactor:调整项目结构

--- server/src/main/java/lazecoding/unique/ServerApplication.java
+++ server/src/main/java/lazecoding/unique/ServerApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class UniqueApplication {
+public class ServerApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(UniqueApplication.class, args);
+        SpringApplication.run(ServerApplication.class, args);
     }
 
 }
