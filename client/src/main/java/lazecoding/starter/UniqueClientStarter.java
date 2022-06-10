47f67a515c0d87f1f91c63442911db0c1fc47598
lazecoding <15062270668@163.com>
2022-06-10 19:34:15 +0800

    feat:client 伴随 configuration 初始化

--- client/src/main/java/lazecoding/starter/UniqueClientStarter.java
+++ client/src/main/java/lazecoding/starter/UniqueClientStarter.java
@@ -2,6 +2,7 @@ package lazecoding.starter;
 
 import lazecoding.config.UniqueClientConfig;
 import lazecoding.api.OpenApi;
+import lazecoding.service.BufferHolder;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -24,6 +25,7 @@ public class UniqueClientStarter {
     public OpenApi openApi(UniqueClientConfig uniqueClientConfig) {
         OpenApi openApi = new OpenApi();
         openApi.init(uniqueClientConfig);
+        BufferHolder.init();
         return openApi;
     }
 
